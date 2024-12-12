4ab2704c40d032275df4f45b561947bb3e189b3b
drake matfield <drakematfield@yahoo.com>
2024-12-12 08:58:27 -0600

    Inital Master Branch Placeholder form.

--- ExerciseRepository/Form1.cs
+++ ExerciseRepository/Form1.cs
@@ -15,5 +15,25 @@ namespace ExerciseRepository
         {
             InitializeComponent();
         }
+
+
+        #region For the Master Branch Placeholding
+        
+        private void aboutToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            string text = @"**Exercise Repository** is a Windows application designed to help users track and manage their workout routine progression.
+
+The project is developed in C# using the .NET Framework 3.5. This repository aims to provide an efficient and meaningful way for users to record and view their exercise data.
+
+While the primary focus is on tracking workout progress, the project is expected to evolve and include additional related features over time.
+
+Please use the development Breach for current devemlopmenting update. 
+
+This about is use as a placeholder as the itnial  Master.";
+
+            MessageBox.Show(text, "Developer Notice", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+        }
+
+        #endregion
     }
 }
