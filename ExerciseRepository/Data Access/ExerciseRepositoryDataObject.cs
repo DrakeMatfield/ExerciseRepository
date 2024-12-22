143f4561d34a2bd6e5e3d44565a0008ec93f857a
drake matfield <drakematfield@yahoo.com>
2024-12-22 06:32:41 -0600

    Initial Implement Data object

--- ExerciseRepository/Data Access/ExerciseRepositoryDataObject.cs
+++ ExerciseRepository/Data Access/ExerciseRepositoryDataObject.cs
@@ -2,10 +2,42 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using ExerciseRepository.Business_Entities;
 
 namespace ExerciseRepository.Data_Access
 {
     public class ExerciseRepositoryDataObject
     {
+        public string FileName { get; set; }
+        public string FileNameWithoutEXT { get; set; } // New property including path without extension
+
+        Bio data;
+
+        public ExerciseRepositoryDataObject()
+        {
+
+        }
+        public ExerciseRepositoryDataObject(string fileName, Bio _data)
+        {
+            FileName = fileName;
+            FileNameWithoutEXT = GetFileNameWithoutExtension(fileName); // Set the new property with path
+
+            this.data = _data;
+        }
+
+        public ExerciseRepositoryDataObject(string fileName)
+        {
+            FileName = fileName;
+            FileNameWithoutEXT = GetFileNameWithoutExtension(fileName); // Set the new property with path
+
+        }
+
+        private string GetFileNameWithoutExtension(string fileName)
+        {
+            // Get the full path without the extension
+            string directory = System.IO.Path.GetDirectoryName(fileName);
+            string fileNameWithoutExtension = System.IO.Path.GetFileNameWithoutExtension(fileName);
+            return System.IO.Path.Combine(directory, fileNameWithoutExtension);
+        }
     }
 }
